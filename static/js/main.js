34f643d00009d1e588778c6dfd8fb3a7659f07fb
Ahmad Alhussin <ahmad.alhusein2@gmail.com>
2022-05-28 01:41:52 +0300

    updated version

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
-const left_panel_img = document.querySelector(".left-panel.panel-image");
-const right_panel_img = document.querySelector(".right-panel.panel-image");
+const left_panel_img = document.querySelector(".left-panel");
+const right_panel_img = document.querySelector(".right-panel");
 const container = document.querySelector(".container");
 
 function sleep(ms) {
@@ -9,16 +9,8 @@ function sleep(ms) {
 }
 sign_up_btn.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
-  sleep(300).then(()=>{
-    left_panel.classList.add("hide-panel");
-    right_panel.classList.remove("hide-panel");
-  });
 });
 
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
-  sleep(300).then(()=>{
-    left_panel.classList.remove("hide-panel");
-    right_panel.classList.add("hide-panel");
-  });
 });
