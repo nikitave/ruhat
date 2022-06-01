a121e3c39e0d6aa5f482cb1d7556a56fc8552398
Daber <88327649+daberL@users.noreply.github.com>
2022-06-01 15:04:58 +0300

    Test of working question Page

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import sys
 
+import flask
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+import json
 
 application = Flask(__name__)
 application.config['SECRET_KEY'] = 'any-secret-key-you-choose'
 
-application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dbUsers.db'
 
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -16,17 +19,24 @@ login_manager = LoginManager(application)
 
 
 ##CREATE TABLE IN DB
-class user(UserMixin, db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
     quizzes = db.Column(db.JSON)
 
+#CREATE TABLE IN DB FOR QUIZES
+class Quiz(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(100))
+    number_of_questions = db.Column(db.Integer)
+    questions = db.Column(db.JSON)
+
 
 @login_manager.user_loader
 def load_user(user_id):
-    return user.query.get(int(user_id))
+    return User.query.get(int(user_id))
 
 
 # Structure for quizzes JSON in User class:
@@ -36,22 +46,23 @@ def load_user(user_id):
 # We need to store id for accessing the quiz from Quiz db - to get questions and answers for them
 # We need to store name for showing it to the Teacher
 
-##CREATE TABLE IN DB FOR QUIZES
-# class quiz(db.Model):
-#     id=db.Column(db.Integer,primary_key=True)
-#     name = db.Column(db.Sgot tring(100))
-#     questions = db.Column(db.JSON)
-#       number_of_questions =...
-
 # Structure for questions:
 # questions = {
 # {text,options,answer}, {text,answer}, ...
 # }
+questionsJson = json.dumps([{"question":"What is 2 + 2?", "options":["2","3","4","5"], "answer":"4"},{"question":"What is 2 * 2?", "options":["2","3","4","5"], "answer":"4"}])
 
+example_quiz = Quiz(name="test",number_of_questions=2,questions=questionsJson)
 
-
-@application.route('/')
+# db.create_all()
+@application.route('/' , methods=["GET", "POST"])
 def home():
+    flask.session['progress'] = 0
+    if request.method == "POST":
+        # print(f"{request.form['username']}",file=sys.stderr)
+        pincode = request.form['pincode']
+        id_quiz = 123123
+        return redirect(url_for('quiz', id_quiz=id_quiz))
     return render_template("startingPage.html")
 
 
@@ -63,7 +74,7 @@ def register(mode):
         # otherwise the user tries to log in
         if len(request.form) == 3:
             # if user already exists, flash them a message
-            if user.query.filter_by(email=request.form['email']).first():
+            if User.query.filter_by(email=request.form['email']).first():
                 flash("We have found your email in our database, try to log in.", 'reg_err')
                 return redirect(url_for('register', mode=mode))
             hash_and_salted_password = generate_password_hash(
@@ -71,7 +82,7 @@ def register(mode):
                 method='pbkdf2:sha256',
                 salt_length=8
             )
-            new_user = user(
+            new_user = User(
                 email=request.form['email'],
                 name=request.form['username'],
                 password=hash_and_salted_password,
@@ -83,13 +94,13 @@ def register(mode):
         else:
             email = request.form.get('email')
             password = request.form.get('password')
-            User = user.query.filter_by(email=email).first()
-            if not User:
+            user = User.query.filter_by(email=email).first()
+            if not user:
                 flash("We can't find your email in our database, please try again.", 'login_err')
                 # print(request.form, file=sys.stderr)
                 return redirect(url_for('register', mode="sign-in-mode"))
-            if check_password_hash(User.password, password):
-                login_user(User)
+            if check_password_hash(user.password, password):
+                login_user(user)
                 return redirect(url_for("quiz_management"))
             else:
                 flash("We can't let you in until you enter the correct password.", 'login_err')
@@ -110,6 +121,22 @@ def quiz_management():
     # You can see your created quizes and can create a new one
     return render_template('quiz_management.html')
 
+
+@application.route('/quiz/<id_quiz>', methods=["GET","POST"])
+def quiz(id_quiz):
+    if request.method=="POST":
+        answer = request.values
+        flask.session['progress']+=1
+        print(answer)
+        if flask.session['progress']==len(json.loads(example_quiz.questions)):
+            return render_template('startingPage.html')
+    print(flask.session['progress'])
+    questions = None
+    game_pincode = flask.session['game_pincode']
+    if game_pincode == "123":
+        questions = json.loads(example_quiz.questions)
+
+    return render_template('questionPage.html', question=questions[flask.session['progress']], id_quiz=id_quiz)
 
 if __name__ == "__main__":
     application.run()
