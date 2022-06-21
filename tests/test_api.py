0a3e24e2c7c2f7267f2e33c2c8035b6ffe216e4d
Daber <88327649+daberL@users.noreply.github.com>
2022-06-21 18:10:33 +0300

    Added another test

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,6 +2,7 @@ import pytest as pytest
 
 from __init__ import create_app
 
+
 @pytest.fixture()
 def client():
     application = create_app()
@@ -16,8 +17,24 @@ def client():
 def test_get_quiz(client):
     id = '123'
     name = 'TestApi'
-    result = client.get(f'/api/get_quiz', query_string= {"name":name,"id":id})
+    result = client.get('/api/get_quiz', query_string={"name": name, "id": id})
     result_json = result.data.decode('utf8').replace("'", '"')
     import json
     data = json.loads(result_json)
-    assert data == [{"answer": "4", "options": ["2", "3", "4", "5"], "question": "What is 2 + 2?"}, {"answer": "a", "options": ["a", "b", "cd", "d"], "question": "dasd"}]
+    assert data == [{"answer": "4", "options": ["2", "3", "4", "5"], "question": "What is 2 + 2?"},
+                    {"answer": "a", "options": ["a", "b", "cd", "d"], "question": "dasd"}]
+
+
+def test_get_result(client):
+    id = '123'
+    name = 'TestApi'
+    result = client.get('/api/get_result', query_string={"id": id, "name": name})
+    result_json = result.data.decode('utf8').replace("'", '"')
+    import json
+    data = json.loads(result_json)
+    assert data == {
+        "correct_answers": 0,
+        "percentage": 0.0,
+        "points": 0,
+        "status": "success"
+    }
